a2dae8d05569a7c12a53e0c37e0c8fdf7275f488
Andrey Sterlyadev <sterlyadev.av@gmail.com>
2022-11-20 15:26:12 +0300

    Complit pp_2.1.5

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,14 @@
 package koschei.models;
 
-public class Egg6 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
+public class Egg6 {
+@Autowired
+private Needle7 needle;
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
